666e547ac88dfb2b68d749d061a8a26a03d3eea7
AlenaTarasenko <tarasenkoalena256@gmail.com>
2019-06-12 11:32:08 +0700

    Initial Commit

---
+++ ANNewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ANNewsApp
+//
+//  Created by Alena Tarasenko on 12/06/2019.
+//  Copyright © 2019 Alena Tarasenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
